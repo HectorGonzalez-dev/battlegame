9bed5f47d39c52b31f68243f8cf6fb2f3f58d7eb
HectorGonzalez-dev <hector.gonzalez.dev@gmail.com>
2025-12-15 18:09:39 +0100

    menu principal hecho

--- display/menu.py
+++ display/menu.py
@@ -1,5 +1,5 @@
 
-def display_menu(header,options,exit):
+def get_menu(header,options,exit):
     cabecera = "+" + "".center(41,"=") + "+\n" +\
                "|" + "".center(41," ") + "|\n" +\
                "|" + header.center(41," ") + "|\n" +\
@@ -16,4 +16,18 @@ def display_menu(header,options,exit):
                 "|" + ("   [0]  {}".format(exit)).ljust(41," ") + "|\n" +\
                 "|" + "".center(41," ") + "|\n" +\
                 "+" + "".center(41,"=") + "+\n\n"
-    print(cabecera+opciones)
+
+    while True:
+        try:
+            print(cabecera+opciones)
+            opc = int(input("Selecciona una opción: "))
+
+            if opc in range(0, len(options)+1):
+                return opc
+            else:
+                print("Opción fuera del rango".center(43, "-"))
+                input("Pulsa enter para continuar\n")
+
+        except ValueError:
+            print("Debes introducir un número entero".center(43, "-"))
+            input("Pulsa enter para continuar\n")
